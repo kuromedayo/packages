411ca8e503ef25325e5cdaab6cdf4144a0f3f2eb
kuromedayo <hunt31999@gmail.com>
2025-02-18 23:00:02 +0900

    commit by backup.sh

--- fonts.scm
+++ fonts.scm
@@ -4,25 +4,6 @@
   #:use-module (guix download)
   #:use-module (guix build-system font))
 
-(define-public font-ubuntu
-  (package
-   (name "font-ubuntu")
-   (version "0.83")
-   (source (origin
-            (method url-fetch)
-            (uri (string-append
-                  "https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-"
-                  version
-                  ".zip"))
-            (sha256
-             (base32
-              "1kwfsvqkkh0928mf75md37g150hs46wqnhzgkzqm5mbga91b78k1"))))
-   (build-system font-build-system)
-   (home-page "https://design.ubuntu.com/font")
-   (synopsis "Ubuntu font family")
-   (description
-    " The Ubuntu font family is a sans-serif typeface family available in 22 styles plus a variable font with adjustable weight and width axes. Its fixed-width companion, Ubuntu Mono, comes in 8 styles and a variable font with an adjustable weight axis. ")
-   (license license:silofl1.1))) ; there is no ubuntu license in licenses.scm
 
 (define-public symbols-nerd-font
   (package
@@ -43,4 +24,24 @@
    (description
     "symbols-nerd-font description dayo")
    (license license:expat)))
+
+(define-public font-ubuntu
+  (package
+   (name "font-ubuntu")
+   (version "0.83")
+   (source (origin
+            (method url-fetch)
+            (uri (string-append
+                  "https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-"
+                  version
+                  ".zip"))
+            (sha256
+             (base32
+              "1kwfsvqkkh0928mf75md37g150hs46wqnhzgkzqm5mbga91b78k1"))))
+   (build-system font-build-system)
+   (home-page "https://design.ubuntu.com/font")
+   (synopsis "Ubuntu font family")
+   (description
+    " The Ubuntu font family is a sans-serif typeface family available in 22 styles plus a variable font with adjustable weight and width axes. Its fixed-width companion, Ubuntu Mono, comes in 8 styles and a variable font with an adjustable weight axis. ")
+   (license license:silofl1.1))) ; there is no ubuntu license in licenses.scm
 
