d80cd6c3e0222eba8559257d8d0d1887463b6933
kuromedayo <hunt31999@gmail.com>
2025-03-24 17:00:03 +0900

    commit by backup.sh

--- fcitx5.scm
+++ fcitx5.scm
@@ -28,7 +28,8 @@
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
   #:use-module (guix build-system copy)
-  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses)
+                #:prefix license:)
   #:use-module (gnu packages anthy)
   #:use-module (gnu packages boost)
   #:use-module (gnu packages compression)
@@ -57,136 +58,48 @@
   #:use-module (gnu packages xml)
   #:use-module (gnu packages xorg))
 
-(define-public fcitx5-catppuccin-theme
-  (let ((commit "383c27ac46cbb55aa5f58acbd32841c1ed3a78a0")
-        (revision "0"))
-    (package
-      (name "fcitx5-catppuccin-theme")
-      (version (git-version "0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/catppuccin/fcitx5")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "07l5d1nlwiq2jhydxckv7a44kic1y8jl93q0jr2i0lmq0zvdzkcz"))))
-      (build-system copy-build-system)
-      (arguments
-       (list
-        #:install-plan
-        #~'(("src" "share/fcitx5/themes"))))
-      (home-page "https://github.com/catppuccin/fcitx5")
-      (synopsis "Soothing pastel theme for Fcitx5")
-      (description
-       "Fcitx5-catppuccin-theme is a soothing pastel theme for Fcitx 5. It provides 4 flavors (latte, frappe, macchiato and mocha) and 14 accent colors (rosewater, flamingo, pink, mauve, red, maroon, peach, yellow, green, teal, sky, sapphire, blue and lavender).")
-      (license license:expat))))
+;; ;; Does not work properly with macOS-light/dark
+;; (define-public fcitx5-themes-candlelight
+;;   (let ((commit "d4146d3d3f7a276a8daa2847c3e5c08de20485da")
+;;         (revision "0"))
+;;     (package
+;;       (name "fcitx5-themes-candlelight")
+;;       (version (git-version "0.1" revision commit))
+;;       (source
+;;        (origin
+;;          (method git-fetch)
+;;          (uri (git-reference
+;;                (url "https://github.com/thep0y/fcitx5-themes-candlelight")
+;;                (commit commit)))
+;;          (file-name (git-file-name name version))
+;;          (sha256
+;;           (base32 "0hm3axrfkd4cxr7shxclw00nk2xxwb2w4b2qjwmps1xkv3mlv1zw"))))
+;;       (build-system copy-build-system)
+;;       (arguments
+;;        (list
+;;         #:install-plan
+;;         #~'(("spring" "share/fcitx5/themes/")
+;;             ("summer" "share/fcitx5/themes/")
+;;             ("autumn" "share/fcitx5/themes/")
+;;             ("winter" "share/fcitx5/themes/")
+;;             ("green" "share/fcitx5/themes/")
+;;             ("transparent-green" "share/fcitx5/themes/")
+;;             ("macOS-light" "share/fcitx5/themes/")
+;;             ("macOS-dark" "share/fcitx5/themes/"))))
+;;       (home-page "https://github.com/thep0y/fcitx5-themes-candlelight")
+;;       (synopsis "Simple theme for fcitx5")
+;;       (description
+;;        "Fcitx5-themes-candlelight is a simple theme for fcitx 5. It has following color variants:
 
-(define-public fcitx5-nord-theme
-  (let ((commit "bdaa8fb723b8d0b22f237c9a60195c5f9c9d74d1")
-        (revision "0"))
-    (package
-      (name "fcitx5-nord-theme")
-      (version (git-version "0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/tonyfettes/fcitx5-nord")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "04bjxzx26x9r01ss7l0fam22nhsvh38dpijy7za0grnr5g93ynm9"))))
-      (build-system copy-build-system)
-      (arguments
-       (list
-        #:install-plan
-        #~'(("Nord-Dark" "share/fcitx5/themes/")
-            ("Nord-Light" "share/fcitx5/themes/"))))
-      (home-page "https://github.com/tonyfettes/fcitx5-nord")
-      (synopsis "Fcitx5 theme based on Nord color")
-      (description
-       "Fcitx5-nord-theme is a Fcitx 5 theme based on Nord Color, featuring dimmed pastel colors for a comfortable yet vibrant ambiance. It offers 2 versions: Nord-dark and Nord-light.")
-      (license license:expat))))
-
-(define-public fcitx5-fluent-theme
-  (let ((commit "399699ac7d366ed6c1952646ed71647e3c8f99b5")
-        (revision "0"))
-    (package
-      (name "fcitx5-fluent-theme")
-      (version (git-version "0.4.0" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/Reverier-Xu/Fluent-fcitx5")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "188gsggh78gjf2kg9kxds819hxidc2yf5aivwbbqayw7cb2h5ijr"))))
-      (build-system copy-build-system)
-      (arguments
-       (list
-        #:install-plan
-        #~'(("FluentDark-solid" "share/fcitx5/themes/")
-            ("FluentDark" "share/fcitx5/themes/")
-            ("FluentLight-solid" "share/fcitx5/themes/")
-            ("FluentLight" "share/fcitx5/themes/"))))
-      (home-page "https://github.com/Reverier-Xu/Fluent-fcitx5")
-      (synopsis "Fluent-Design theme with blur effect and shadow for fcitx5")
-      (description
-       "fcitx5-fluent-theme is a Fluent-Design theme with blur effect and shadow. It offers 4 versions: FluentDark, FluentDark-solid, FluentLight, and FluentLight-solid.")
-      (license license:mpl2.0))))
-
-;; Does not work properly with macOS-light/dark
-(define-public fcitx5-themes-candlelight
-  (let ((commit "d4146d3d3f7a276a8daa2847c3e5c08de20485da")
-        (revision "0"))
-    (package
-      (name "fcitx5-themes-candlelight")
-      (version (git-version "0.1" revision commit))
-      (source
-       (origin
-         (method git-fetch)
-         (uri (git-reference
-               (url "https://github.com/thep0y/fcitx5-themes-candlelight")
-               (commit commit)))
-         (file-name (git-file-name name version))
-         (sha256
-          (base32
-           "0hm3axrfkd4cxr7shxclw00nk2xxwb2w4b2qjwmps1xkv3mlv1zw"))))
-      (build-system copy-build-system)
-      (arguments
-       (list
-        #:install-plan
-        #~'(("spring" "share/fcitx5/themes/")
-            ("summer" "share/fcitx5/themes/")
-            ("autumn" "share/fcitx5/themes/")
-            ("winter" "share/fcitx5/themes/")
-            ("green" "share/fcitx5/themes/")
-            ("transparent-green" "share/fcitx5/themes/")
-            ("macOS-light" "share/fcitx5/themes/")
-            ("macOS-dark" "share/fcitx5/themes/"))))
-      (home-page "https://github.com/thep0y/fcitx5-themes-candlelight")
-      (synopsis "Simple theme for fcitx5")
-      (description
-       "Fcitx5-themes-candlelight is a simple theme for fcitx 5. It has following color variants:
-
-@itemize
-@item spring
-@item summer
-@item autumn
-@item winter
-@item green
-@item transparent-green
-@item macOS-light
-@item macOS-dark")
-      (license license:expat))))
-
-
-
+;; @itemize
+;; @item spring
+;; @item summer
+;; @item autumn
+;; @item winter
+;; @item green
+;; @item transparent-green
+;; @item macOS-light
+;; @item macOS-dark
+;; @end itemize\n")
+;;       (license license:expat))))
 
